4f2f9976a139c28b2bbc74626cee1f4fcf452aa7
olzone <osurman@gmail.com>
2016-03-07 12:21:04 +0100

    Add simple solar system sample

---
+++ Samples/Simple solar system/Assets/orbit.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class orbit : MonoBehaviour {
+
+    public int r;
+	void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 v = transform.position;
+
+        transform.up = transform.position.normalized;
+
+        GetComponent<Rigidbody>().velocity = transform.forward * r;
+        
+
+        if(Vector3.Distance(v, new Vector3(0,0,0)) > r)
+        {
+            v = v.normalized * (float)r;
+        }
+
+        transform.position = v;
+    }
+}
