72944c4260faf0e1efad7a62fce0938a10b5aa21
Sepioł <ssepiol1993@gmail.com>
2016-05-31 01:08:43 +0200

    Simple menu scene

---
+++ Main/Assets/Scripts/StartNewGameScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartNewGameScript : MonoBehaviour {
+
+	// Update is called once per frame
+	public void ChangeScene () {
+		Application.LoadLevel("Proc_planet");
+	}
+		
+}
